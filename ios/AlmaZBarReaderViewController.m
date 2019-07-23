fba17117757afb912aaee0ea743d0059ed7561c3
Marisa Barradas <marisa.barradas@create.pt>
2019-07-23 16:04:15 +0100

    45

--- ios/AlmaZBarReaderViewController.m
+++ ios/AlmaZBarReaderViewController.m
@@ -74,7 +74,7 @@
         // polygonView.center = self.scanReader.view.center;
         polygonView.layer.borderColor = [UIColor whiteColor].CGColor;
         polygonView.layer.borderWidth = 2.0f;
-		yourView.layer.cornerRadius = 20.0f;
+		polygonView.layer.cornerRadius = 20.0f;
 
         // UIView *lineView = [[UIView alloc] initWithFrame:CGRectMake(0,dim / 2, dim, 1)];
         // lineView.backgroundColor = [UIColor orangeColor];
